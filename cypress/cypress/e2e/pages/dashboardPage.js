1ca695298bb90aafceec41aab737ce385150e3ff
camila_guayara <c.guayara@uniandes.edu.co>
2024-11-09 18:08:01 -0500

    archivos kraken

--- cypress/cypress/e2e/pages/dashboardPage.js
+++ cypress/cypress/e2e/pages/dashboardPage.js
@@ -7,6 +7,17 @@ class DashboardPage {
         cy.wait(1000);
         this.membersLink.click();
     }
+
+    get postLink(){
+        return cy.get('li>a[href="#/posts/"]');
+    }
+    
+    clickMembersLink(){
+        cy.wait(1000);
+        this.postLink.click();
+    }
+
+
 }
 
 export default new DashboardPage();
